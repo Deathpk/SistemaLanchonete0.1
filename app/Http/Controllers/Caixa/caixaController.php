765031695549116bae51d1c8a14b92a19dcec41c
Michel <bettercallmiguel@gmail.com>
2020-06-04 17:47:26 -0300

    Refatorando...

--- app/Http/Controllers/Caixa/caixaController.php
+++ app/Http/Controllers/Caixa/caixaController.php
@@ -15,34 +15,12 @@ class caixaController extends Controller
     
     //mostra a view do caixa.
     public function show(){
-    
-            return view('caixa.caixa');
-            
+        return view('caixa.caixa');
         }
 
         public function showItens(Request $request){
-
+            
             $itemSelected = productModel::where('type', '=', $request->Prodtype)->get('*'); // Seleciona todos os itens com o msm tipo do requisitado do formulário.
             return view('caixa.itensView', compact('itemSelected'));
-
             }
-            
-            public function currentTransaction(Request $request){
-
-                $product = $request->produto; // ID do produto selecionado.
-                //dd($product);
-
-                $itemName = productModel::where('id', '=', $product)->get('name')->toArray();// pega o nome do produto selecionado por id 
-                $itemPrice = productModel::where('id', '=', $product)->get('price')->toArray(); // pega o preço do produto selecionado por id e converte para array , para ser usado na variavel global subtotal.
-                //dd($itemPrice);
-
-                
-                
-            }
-              
-            
-
-           /* public function subTotal($item){
-                $item = $prodPrice + $
-            }*/
 }
